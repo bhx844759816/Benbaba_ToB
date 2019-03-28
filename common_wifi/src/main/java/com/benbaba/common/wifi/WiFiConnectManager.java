7802cafee3648eec6fe51a988cc15a5bd12a2331
lmr <1483587277@qq.com>
2019-03-28 17:20:41 +0800

    wifi

---
+++ common_wifi/src/main/java/com/benbaba/common/wifi/WiFiConnectManager.java
@@ -0,0 +1,147 @@
+package com.benbaba.common.wifi;
+
+import android.content.Context;
+import android.net.wifi.ScanResult;
+import android.net.wifi.WifiConfiguration;
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
+import android.text.TextUtils;
+import android.util.Log;
+
+import com.benbaba.common.wifi.wifi.WiFiConnect;
+
+import java.util.List;
+
+/**
+ * Created by code4a on 2017/5/16.
+ */
+
+public final class WiFiConnectManager {
+
+    private Context mContext;
+    private WifiManager mWifiManager;
+
+    /**
+     * 构造方法
+     *
+     * @param mContext 上下文对象
+     */
+    public WiFiConnectManager(Context mContext) {
+        this.mContext = mContext;
+        mWifiManager = (WifiManager) mContext.getSystemService(Context.WIFI_SERVICE);
+        startScan();
+    }
+
+    /**
+     * 开始扫描WiFi
+     */
+    public void startScan() {
+        mWifiManager.startScan();
+    }
+
+    /**
+     * 获取扫描结果
+     *
+     * @return 扫描结果
+     */
+    public List<ScanResult> getScanResults() {
+        return mWifiManager.getScanResults();
+    }
+
+    public String  getCurrentWifiInfo(){
+       return mWifiManager.getConnectionInfo().getSSID();
+    }
+
+    /**
+     * 连接到指定SSID名称的WiFi热点上
+     *
+     * @param ssid                wifi名称
+     * @param password            wifi密码
+     * @param wiFiConnectListener 连接监听
+     */
+    public void connectWiFi(String ssid, String password, WiFiConnectListener wiFiConnectListener) {
+        ScanResult scanResult = getTargetScanResultBySSID(ssid);
+        if (scanResult == null && wiFiConnectListener != null) {
+            wiFiConnectListener.connectStart();
+            wiFiConnectListener.connectResult(ssid, false);
+            wiFiConnectListener.connectEnd();
+        } else {
+            connectWiFi(scanResult, password, wiFiConnectListener);
+        }
+    }
+
+
+
+    /**
+     * 连接到指定ScanResult的WiFi热点上
+     *
+     * @param mScanResult         wifi的扫描结果
+     * @param password            wifi密码
+     * @param wiFiConnectListener 连接监听
+     */
+    public void connectWiFi(ScanResult mScanResult, String password, WiFiConnectListener wiFiConnectListener) {
+        if (wiFiConnectListener != null) {
+            wiFiConnectListener.connectStart();
+        }
+        final WifiConfiguration config = WiFiConnect.getWifiConfiguration(mWifiManager, mScanResult);
+        if (config == null) {
+            boolean connectResult = WiFiConnect.newNetworkToConnect(mContext, mWifiManager, mScanResult, password);
+            if (wiFiConnectListener != null) {
+                wiFiConnectListener.connectResult(mScanResult.SSID, connectResult);
+            }
+        } else {
+            final boolean isCurrentNetwork_ConfigurationStatus = config.status == WifiConfiguration.Status.CURRENT;
+            final WifiInfo info = mWifiManager.getConnectionInfo();
+            final boolean isCurrentNetwork_WifiInfo = info != null
+                    && TextUtils.equals(info.getSSID(), mScanResult.SSID)
+                    && TextUtils.equals(info.getBSSID(), mScanResult.BSSID);
+            if (isCurrentNetwork_ConfigurationStatus || isCurrentNetwork_WifiInfo) {
+                if (wiFiConnectListener != null) {
+                    wiFiConnectListener.connectResult(mScanResult.SSID, true);
+                }
+            } else {
+                boolean forgetResult = WiFiConnect.forgetCurrentNetwork(mWifiManager, mScanResult);
+                Log.i("TAG", "forgetResult:" + forgetResult);
+                boolean connectResult = WiFiConnect.configNetworkToConnect(mContext, mWifiManager, mScanResult);
+                if (wiFiConnectListener != null) {
+                    wiFiConnectListener.connectResult(mScanResult.SSID, connectResult);
+                }
+            }
+        }
+        if (wiFiConnectListener != null) {
+            wiFiConnectListener.connectEnd();
+        }
+    }
+
+    /**
+     * 根据SSID名称 获取到ScanResult
+     *
+     * @param ssid wifi名称
+     * @return 扫描结果
+     */
+    ScanResult getTargetScanResultBySSID(String ssid) {
+        ScanResult sr = null;
+        List<ScanResult> wifiList = getScanResults();
+        if (wifiList == null || TextUtils.isEmpty(ssid)) {
+            return null;
+        }
+        for (int i = 0; i < wifiList.size(); i++) {
+            if (ssid.equals(wifiList.get(i).SSID)) {
+                sr = wifiList.get(i);
+                break;
+            }
+        }
+        return sr;
+    }
+
+    /**
+     * wifi连接监听
+     */
+    public interface WiFiConnectListener {
+        void connectStart();
+
+        void connectResult(String ssid, boolean isSuccess);
+
+        void connectEnd();
+    }
+}
