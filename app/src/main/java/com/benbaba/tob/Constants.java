1e05236dece5a193ee73112a9fd83b53702f704a
bhx <2466921206@qq.com>
2019-03-29 18:09:43 +0800

    编写首页配置界面 和Tcp服务端部分

--- app/src/main/java/com/benbaba/tob/Constants.java
+++ app/src/main/java/com/benbaba/tob/Constants.java
@@ -10,5 +10,6 @@ public class Constants {
     public static final String DEVICE_IP_ADDRESS = "255.255.255.255";
     public static final int DEVICE_PORT = 10025;
     public static final int RECEIVE_PORT = 10026;
-
+    public static String[] tones = new String[]{"底鼓", "军鼓", "嗵鼓1", "嗵鼓2", "踩镲", "吊镲", "do", "re", "mi", "fa", "so", "la", "si", "do+"};
+    public static String[] mode = new String[]{"非洲鼓", "键盘"};
 }
